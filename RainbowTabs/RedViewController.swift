42347fa15cb2e0b735270f4ba558334485dd69a9
tali-bd <taliblando@gmail.com>
2025-04-25 11:54:48 -0700

    Rainbow Tabs Complete
    All tabs added with custom view controllers for each tab

--- RainbowTabs/RedViewController.swift
+++ RainbowTabs/RedViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tabBarItem.badgeValue = "!"
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        tabBarItem.badgeValue = nil
+    }
 
 }
 
